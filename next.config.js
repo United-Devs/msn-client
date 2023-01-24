f3ced7a7ef1b41a91bd947615d772bc2b4233be3
Marcos Mendes <marcosdev.me@gmail.com>
2023-01-24 16:08:20 -0300

    chore: remove next-pwa

--- next.config.js
+++ next.config.js
@@ -1,15 +1,3 @@
-// eslint-disable-next-line @typescript-eslint/no-var-requires
-const withPWA = require('next-pwa')
-const isProduction = process.env.NODE_ENV === 'production'
-
-module.exports = withPWA({
-  swcMinify: true,
-  compiler: {
-    // Enables the styled-components SWC transform
-    styledComponents: true
-  },
-  pwa: {
-    dest: 'public',
-    disable: !isProduction
-  }
-})
+module.exports = {
+  swcMinify: true
+}
